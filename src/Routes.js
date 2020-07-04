15eaca2e0db186c6214f10e7c0f73d183f105609
Francis Badasu <nytro04@gmail.com>
2020-07-04 18:19:47 +0000

    folder structure setup

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const Routes = () => {
+  return (
+    <div>
+      <Route exact path="/" component={HomePage} />
+    </div>
+  );
+};
+
+export default Routes;
