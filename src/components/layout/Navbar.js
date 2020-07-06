11f3db1319d715b0f398138b207c1409dc4aecc6
Francis Badasu <nytro04@gmail.com>
2020-07-06 20:31:59 +0000

    got tachyons working

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,14 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-dark-red">
+    <nav className="flex justify-around mt3">
       <span>logo goes here</span>
 
-      <ul className="flex list">
+      <ul className="flex justify-between list">
         <li className="mb6">call me (+233549970041)</li>
         {/* <li>/</li> */}
         <li>nytro04@gmail.com</li>
       </ul>
+      <span>menu</span>
     </nav>
   );
 };
