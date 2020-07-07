e4509d6285c932d3b8efb160561aaf4773f785d6
Francis Badasu <nytro04@gmail.com>
2020-07-07 20:31:49 +0000

    add header initial set up

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -5,6 +5,17 @@ const Header = () => {
   return (
     <div>
       <Navbar />
+      <div className="flex mt6 justify-around items-center">
+        <div>
+          <h1 className="">Francis Badasu</h1>
+          <h3>Frontend Developer</h3>
+          <h5 className="mv4">
+            I love to translate designs into clean, elegant, functional and
+            responsive User Interfaces.
+          </h5>
+        </div>
+        <div>black illustration goes here</div>
+      </div>
     </div>
   );
 };
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,14 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-dark-red">
+    <nav className="flex justify-around mt2 ttu">
       <span>logo goes here</span>
 
       <ul className="flex list">
-        <li className="mb6">call me (+233549970041)</li>
-        {/* <li>/</li> */}
+        <li className="">call me +233549970041</li>
+        <li className="mh3">/</li>
         <li>nytro04@gmail.com</li>
       </ul>
+      <span>menu bar</span>
     </nav>
   );
 };
