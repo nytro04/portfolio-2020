501ac9b67bada71c6b569345ead20c41d926da53
Francis Badasu <nytro04@gmail.com>
2020-07-08 05:10:20 +0000

    fixes: merge second fix

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,17 +3,6 @@ import Menu from "../Menu";
 
 const Navbar = () => {
   return (
-<<<<<<< HEAD
-    <nav className="flex justify-around mt3">
-      <span>logo goes here</span>
-
-      <ul className="flex justify-between list">
-        <li className="mb6">call me (+233549970041)</li>
-        {/* <li>/</li> */}
-        <li>nytro04@gmail.com</li>
-      </ul>
-      <span>menu</span>
-=======
     <nav className="flex justify-around mt2 ttu">
       <span>logo goes here</span>
 
@@ -22,14 +11,10 @@ const Navbar = () => {
         <li className="mh3">/</li>
         <li>nytro04@gmail.com</li>
       </ul>
-<<<<<<< HEAD
-      <span>menu bar</span>
->>>>>>> develop
-=======
+
       <span>
         <Menu />
       </span>
->>>>>>> develop
     </nav>
   );
 };
