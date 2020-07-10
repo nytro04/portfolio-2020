e07f02791212ee05e6faa5091b8e5de550c32a18
Francis Badasu <nytro04@gmail.com>
2020-07-10 20:57:04 +0000

    add change profile svg

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Navbar from "../layout/Navbar";
+import { ReactComponent as ProfileSvg } from "../../assets/img/me.svg";
+
+// import { ReactComponent as PhoneImg } from "../../assets/PHONE/PHONE.svg";
 
 const Header = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex mt6 justify-around items-center">
-        <div>
+      <div className="mw9 center ph3-ns">
+        <div className="cf ph2-ns">
+          <div className="fl w-100 w-50-ns">
+            <h1 className="">Francis Badasu</h1>
+            <h3>Frontend Developer</h3>
+            <h5 className="mv4">
+              I love to translate designs into clean, elegant, functional and
+              responsive User Interfaces.
+            </h5>
+          </div>
+          <div className="fl w-100 w-50-ns"></div>
+        </div>
+        <ProfileSvg />
+        {/* <img src={profileSvg} alt="profile illustration" /> */}
+        {/* <div className="flex mt6 justify-around items-center">
           <h1 className="">Francis Badasu</h1>
           <h3>Frontend Developer</h3>
           <h5 className="mv4">
@@ -14,7 +30,7 @@ const Header = () => {
             responsive User Interfaces.
           </h5>
         </div>
-        <div>black illustration goes here</div>
+        <div>black illustration goes here</div> */}
       </div>
     </div>
   );
