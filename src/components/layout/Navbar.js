3d1b949625aaa9b5e0cd4c2177f6909c1ee898d7
Francis Badasu <nytro04@gmail.com>
2020-07-11 20:57:28 +0000

    change menu toggler

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../Menu";
 
 const Navbar = () => {
-  return (
-    <nav className="flex justify-around mt2 ttu">
-      <span>logo goes here</span>
+  const [menuOpen, setmenuOpen] = useState(false);
 
-      <ul className="flex list">
-        <li className="">call me +233549970041</li>
-        <li className="mh3">/</li>
-        <li>nytro04@gmail.com</li>
-      </ul>
-      <span>
-        <Menu />
-      </span>
-    </nav>
-  );
+  const linksArray = [
+    { name: "home", url: "#" },
+    { name: "log in", url: "#" },
+    { name: "photos", url: "#" },
+    { name: "contact", url: "#" },
+    { name: "download", url: "#" },
+  ];
+
+  const socialArray = [
+    {
+      icon: "fa fa-github-square",
+      url: "https://github.com/matthewvincent",
+    },
+    {
+      icon: "fa fa-instagram",
+      url: "https://www.instagram.com/middlestates/",
+    },
+    {
+      icon: "fa fa-tumblr-square",
+      url: "http://matthewvincentphotography.com/",
+    },
+  ];
+
+  return <nav className="flex justify-around mt2 ttu"></nav>;
 };
 
 export default Navbar;
